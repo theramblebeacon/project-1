819e7eb726908973a034d41b9e7011ea28776815
Brendon Potts <brendon.potts@gmail.com>
2018-07-12 15:51:28 -0700

    made many minor changes to all files

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,10 @@
-var myImage = document.querySelector('img');
+var myImage = document.querySelector ('img');
 
-myImage.onclick = function() {
-    var mySrc = myImage.getAttribute('src');
+myImage.onclick = function () {
+    var mySrc = myImage.getAttribute ('src');
     if(mySrc === 'images/crestovernight.jpg') {
       myImage.setAttribute ('src','images/crestovernight-3.jpg');
     } else {
       myImage.setAttribute ('src','images/crestovernight.jpg');
     }
-}
-var myButton = document.querySelector('button');
-var myHeading = document.querySelector('h3');
-function setUserName() {
-  var myName = prompt('Please enter your name.');
-  localStorage.setItem('name', myName);
-  myHeading.textContent = 'Mozilla is cool, ' + myName;
-}
-if(!localStorage.getItem('name')) {
-  setUserName();
-} else {
-  var storedName = localStorage.getItem('name');
-  myHeading.textContent = 'Mozilla is cool, ' + storedName;
-}
-myButton.onclick = function() {
-  setUserName();
 }
